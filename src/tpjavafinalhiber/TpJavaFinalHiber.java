609207c561649dc9044228cdf99c86981f076af0
Nippur7 <nippur.argento@gmail.com>
2023-12-03 14:49:48 -0300

    Add Match (partido)

--- src/tpjavafinalhiber/TpJavaFinalHiber.java
+++ src/tpjavafinalhiber/TpJavaFinalHiber.java
@@ -6,7 +6,10 @@
 package tpjavafinalhiber;
 
 import java.io.IOException;
-import java.sql.Date;
+import java.text.ParseException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -31,24 +34,25 @@ public class TpJavaFinalHiber
 	int puntos;
 	int partidos;
 	int idpartido;
-	Date fecha;
-	int idequipo1;
-	int idequipo2;
-	int golesEq1;
-	int golesEq2;
+	static Date fecha;
+	static int idequipo1;
+	static int idequipo2;
+	static int golesEq1;
+	static int golesEq2;
 	
-	public static void main(String[] args) throws IOException 
+	public static void main(String[] args) throws IOException, ParseException 
         {
 		
 		CRUDOp crud = new CRUDOp();
                 Logger.getLogger("org.hibernate").setLevel(Level.OFF);                
 			Scanner scan = new Scanner(System.in);
 			Scanner scanText = new Scanner(System.in);
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 			int opcion = 1;			
 			while(opcion == 1) 
                         {
 				System.out.println("Menú CRUD --Equipos--");
-				System.out.println("1) Agregar Equipo\n2) Recuperar datos del Equipo\n3) Actualizar un Equipo\n4) Eliminar un Equipo\n");
+				System.out.println("1) Agregar Equipo\n2) Recuperar datos del Equipo\n3) Actualizar un Equipo\n4) Eliminar un Equipo\n5) Agregar un Partido");
 				System.out.println("Teclee una opción:");
 				scanText.useDelimiter("\\n");			
 				int selector = scan.nextInt();			
@@ -110,6 +114,21 @@ public class TpJavaFinalHiber
                                             }
                                             
 						break;
+					case 5:
+						System.out.println("Agregar un Partido\n");
+						System.out.println("Ingrese ID del equipo Local\n");
+                                                idequipo1 = scan.nextInt();
+                                                System.out.println("Ingrese ID del equipo Visitante\n");
+                                                idequipo2 = scan.nextInt();
+						System.out.println("Ingrese Fecha del Encuentro\n");
+                                                String fechaComoTexto = scanText.nextLine();
+                                                fecha = sdf.parse(fechaComoTexto);                                                
+						System.out.println("Ingrese CANTIDAD DE Goles Local\n");
+						golesEq1 = scan.nextInt();
+						System.out.println("Ingrese CANTIDAD DE Goles Visitante\n");
+						golesEq2 = scan.nextInt();
+						crud.agregarPartido(idequipo1, idequipo2, fecha, golesEq1, golesEq2);
+						break;                                            
 					default:
 						System.out.println("Por favor teclee alguna opción válida\n");
 			
